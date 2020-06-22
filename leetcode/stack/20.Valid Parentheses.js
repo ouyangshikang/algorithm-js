20e77a4148a8b9b88cf403795a911b6380ec6b08
baituo <baituo@gaoding.com>
2020-06-22 22:37:58 +0800

    feat: 20.Valid Parentheses

---
+++ leetcode/stack/20.Valid Parentheses.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isValid = function(s) {
+    let stack = [];
+    const left = ['(', '{', '['];
+    const right = [')', '}', ']'];
+
+    for(let i = 0; i < s.length; i++) {
+        const curStr = s[i];
+
+        if(left.includes(curStr)) {
+            stack.push(curStr);
+        }
+        else if(right.includes(curStr)) {
+            const popStr = stack.pop();
+
+            if(
+                !popStr ||
+                (popStr === '(' && curStr !== ')') ||
+                (popStr === '{' && curStr !== '}') ||
+                (popStr === '[' && curStr !== ']')
+            ) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
